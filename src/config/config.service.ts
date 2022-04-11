355d8294b2510e22ee09f60dcfc23a82db62aa0e
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-04-11 19:46:18 +0200

    feat: add swagger

--- src/config/config.service.ts
+++ src/config/config.service.ts
@@ -5,6 +5,10 @@ import { ConfigService as NestConfigService } from '@nestjs/config';
 export class ConfigService {
   public constructor(private readonly nestConfigService: NestConfigService) {}
 
+  public get version(): string {
+    return process.env.npm_package_version || '';
+  }
+
   public get serverPort(): number {
     return this.nestConfigService.get('SERVER_PORT', 3000);
   }
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
@@ -7,6 +8,12 @@ import { ConfigService } from './config/config.service';
 async function bootstrap() {
   const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const config = app.get(ConfigService);
+
+  const swaggerConfig = new DocumentBuilder().setTitle('Zion API').setVersion(config.version).build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('explorer', app, document);
+
   await app.listen(config.serverPort, '0.0.0.0');
 }
+
 bootstrap();
