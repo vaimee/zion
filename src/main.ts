93a582b80e43cc951032c6c5829a817d1b5c697b
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-04-11 19:39:13 +0200

    feat: switch to fastify

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,12 @@
 import { NestFactory } from '@nestjs/core';
+import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify';
 
 import { AppModule } from './app.module';
 import { ConfigService } from './config/config.service';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
   const config = app.get(ConfigService);
-  await app.listen(config.serverPort);
+  await app.listen(config.serverPort, '0.0.0.0');
 }
 bootstrap();
