059121528745249620d83a444836e461e8ca64b0
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-05-20 17:28:22 +0200

    feat: add basic implementation of the abstract repository

--- src/persistence/abstract.repository.ts
+++ src/persistence/abstract.repository.ts
@@ -1,32 +1,44 @@
-import { InjectKnex, Knex } from 'nestjs-knex';
+import { Knex } from 'nestjs-knex';
 
 import { Model } from '../common/models/model';
 import { Repository } from './repository.interface';
 
 export abstract class AbstractRepository<T extends Model> implements Repository<T> {
-  public constructor(@InjectKnex() protected readonly knex: Knex, private readonly namespace: string) {}
+  public constructor(protected readonly knex: Knex, private readonly namespace: string) {}
 
-  public create(item: T): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async create(item: T): Promise<T> {
+    const trx = await this.knex.transaction();
+    try {
+      const ids = await trx(this.namespace).insert(item, 'id');
+      const res = await trx(this.namespace).select('*').where({ id: ids[0] }).first();
+      await trx.commit();
+      return res;
+    } catch (error) {
+      await trx.rollback(error);
+      throw error;
+    }
   }
 
-  public update(id: number, item: Partial<T>): Promise<void> {
-    throw new Error('Method not implemented.');
+  public async update(id: number, item: Partial<T>): Promise<void> {
+    return this.knex(this.namespace).where({ id }).update(item);
   }
 
-  public delete(id: number): Promise<void> {
-    throw new Error('Method not implemented.');
+  public async delete(id: number): Promise<void> {
+    return this.knex(this.namespace).where({ id }).del();
   }
 
-  public find(item: Partial<T>): Promise<T[]> {
-    throw new Error('Method not implemented.');
+  public async find(item: Partial<T>): Promise<T[]> {
+    return this.knex(this.namespace).select('*').where(item);
   }
 
-  public findOne(id: number | Partial<T>): Promise<T> {
-    throw new Error('Method not implemented.');
+  public async findOne(id: number | Partial<T>): Promise<T> {
+    const filter = typeof id === 'number' ? { id } : id;
+    return this.knex(this.namespace).select('*').where(filter).first();
   }
 
-  public exist(id: number | Partial<T>): Promise<boolean> {
-    throw new Error('Method not implemented.');
+  public async exist(id: number | Partial<T>): Promise<boolean> {
+    const filter = typeof id === 'number' ? { id } : id;
+    const res = await this.knex(this.namespace).count('id as count').where(filter);
+    return res[0]['count'] > 0;
   }
 }
