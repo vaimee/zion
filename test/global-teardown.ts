e3c0fe062c57e46189b02ef4fe8a3e9eb82597a7
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-06-09 18:37:10 +0200

    test: clean up

--- test/global-teardown.ts
+++ test/global-teardown.ts
@@ -2,12 +2,7 @@ import dockerCompose from 'docker-compose';
 import isCI from 'is-ci';
 
 export default async function () {
-  console.time('global-teardown');
-  console.log('\n');
-
   if (isCI) {
     dockerCompose.down();
   }
-
-  console.timeEnd('global-teardown');
 }
