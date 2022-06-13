da6459ca8afae0cea5aa509cdf1a060d66bf331c
reluc <relu.cri@gmail.com>
2022-06-13 12:37:18 +0200

    test: randomize app test port

--- test/utils/resources.ts
+++ test/utils/resources.ts
@@ -2,20 +2,18 @@ import { Test } from '@nestjs/testing';
 import ax from 'axios';
 
 import { AppModule } from '../../src/app.module';
-import { ConfigService } from '../../src/config/config.service';
 
 export async function getE2ETestResources() {
   const moduleFixture = await Test.createTestingModule({
     imports: [AppModule],
   }).compile();
   const app = moduleFixture.createNestApplication();
-  const config = app.get(ConfigService);
 
   await app.init();
-  await app.listen(config.serverPort);
-
+  await app.listen(0);
+  const url = await app.getUrl();
   const axios = ax.create({
-    baseURL: `http://127.0.0.1:${config.serverPort}`,
+    baseURL: url,
     validateStatus: () => true,
   });
 
