5dac3b2fe2a4180d52bd7e51c28b6263098d15d3
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-06-13 19:58:37 +0200

    feat: add thing_description table

--- migrations/20220518171107_init.ts
+++ migrations/20220518171107_init.ts
@@ -1,12 +1,24 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('user', (table) => {
-    table.increments('id').primary();
-    table.string('email').unique().notNullable();
-    table.string('password').notNullable();
-    table.timestamps(true, true);
-  });
+  return knex.schema
+    .createTable('user', (table) => {
+      table.increments('id').primary();
+      table.string('email').unique().notNullable();
+      table.string('password').notNullable();
+      table.timestamps(true, true);
+    })
+    .createTable('thing_description', (table) => {
+      table.increments('id').primary();
+      table.string('urn').unique().notNullable();
+      table.jsonb('json').notNullable();
+      table.timestamp('created');
+      table.timestamp('modified');
+      table.timestamp('expires');
+      table.integer('ttl');
+      table.timestamp('retrieved');
+      table.integer('owner_id').references('id').inTable('user').notNullable();
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
