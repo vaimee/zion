5dc665df7c6818813beaa735f1d53c82d1636e6d
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-06-14 10:42:24 +0200

    feat: add extra formats to ajv for thing description validation

--- src/common/utils/thing-description-validator.ts
+++ src/common/utils/thing-description-validator.ts
@@ -1,9 +1,14 @@
 import Ajv from 'ajv';
+import addFormats from 'ajv-formats';
+import * as addFormatsDraft from 'ajv-formats-draft2019';
 import * as schema from 'wot-thing-description-types/schema/td-json-schema-validation.json';
 
 import { ValidationError } from './../interfaces/validation-error';
 
 const ajv = new Ajv({ strict: false });
+addFormats(ajv);
+addFormatsDraft(ajv);
+
 const validate = ajv.compile(schema);
 
 export function validateThingDescription(data: unknown): { valid: boolean; errors?: ValidationError[] } {
