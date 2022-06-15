d7e70f124f5519c8a02c48d722e16ba122c72417
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-06-15 10:57:00 +0200

    test: add tests for PUT /things/:id

--- test/e2e/things.e2e-spec.ts
+++ test/e2e/things.e2e-spec.ts
@@ -3,6 +3,7 @@ import { AxiosInstance } from 'axios';
 
 import { User } from './../../src/common/models/user';
 import { getAccessToken } from './../utils/auth';
+import { getShortUnique } from './../utils/data';
 import { createUser } from './../utils/database';
 import * as invalidThingDescription from './../utils/tds/invalid.td.json';
 import * as validThingDescription from './../utils/tds/valid.td.json';
@@ -72,6 +73,47 @@ describe('/things', () => {
         const { status, data } = await axios.get(headers.location);
 
         expect(status).toBe(200);
+        expect(data).toStrictEqual(validThingDescription);
+      });
+    });
+
+    describe('PUT', () => {
+      it('should fail to update/create the Thing Description when it is invalid', async () => {
+        const { status, data } = await axios.put('/things/no-matter', invalidThingDescription, {
+          headers: { Authorization: `Bearer ${defaultAccessToken}` },
+        });
+
+        expect(status).toBe(400);
+        expect(data.validationErrors).toStrictEqual([
+          { field: '/properties/status', description: "must have required property 'forms'" },
+        ]);
+      });
+
+      it('should update the Thing Description', async () => {
+        const { headers } = await axios.post('/things', validThingDescription, {
+          headers: { Authorization: `Bearer ${defaultAccessToken}` },
+        });
+        const updatedThingDescription = { ...validThingDescription, newField: 'test' };
+
+        const { status } = await axios.put(headers.location, updatedThingDescription, {
+          headers: { Authorization: `Bearer ${defaultAccessToken}` },
+        });
+
+        const { data } = await axios.get(headers.location);
+        expect(status).toBe(204);
+        expect(data).toStrictEqual(updatedThingDescription);
+      });
+
+      it('should create the Thing Description', async () => {
+        const id = getShortUnique();
+
+        const { status } = await axios.put(`/things/${id}`, validThingDescription, {
+          headers: { Authorization: `Bearer ${defaultAccessToken}` },
+        });
+
+        const { data } = await axios.get(`/things/${id}`);
+
+        expect(status).toBe(201);
         expect(data).toStrictEqual(validThingDescription);
       });
     });
