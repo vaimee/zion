3fad4cf512ef03a40f78f35a52131051a41cd906
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-06-16 09:49:59 +0200

    refactor: remove common module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,12 @@ import { Module } from '@nestjs/common';
 
 import { ApiReferenceModule } from './api-reference/api-reference.module';
 import { AuthModule } from './auth/auth.module';
-import { CommonModule } from './common/common.module';
 import { ConfigModule } from './config/config.module';
 import { IntroductionModule } from './introduction/introduction.module';
 import { PersistenceModule } from './persistence/persistence.module';
 
 @Module({
-  imports: [ConfigModule, CommonModule, PersistenceModule, IntroductionModule, AuthModule, ApiReferenceModule],
+  imports: [ConfigModule, PersistenceModule, IntroductionModule, AuthModule, ApiReferenceModule],
   controllers: [],
   providers: [],
 })
