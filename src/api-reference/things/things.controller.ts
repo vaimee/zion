49412daf574488a88117d4d68a040474e1edd3be
Lorenzo Gigli <lorenzo.dramis@gmail.com>
2022-06-17 15:38:03 +0200

    refactor: move the manipulation of request/response from things service to the controller

--- src/api-reference/things/things.controller.ts
+++ src/api-reference/things/things.controller.ts
@@ -1,4 +1,19 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post, Put, Query, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  BadRequestException,
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+  Put,
+  Query,
+  Req,
+  Res,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { FastifyRequest as Request, FastifyReply as Response } from 'fastify';
 
@@ -6,6 +21,7 @@ import { AuthGuard } from './../../auth/auth.guard';
 import { CurrentUser } from './../../common/decorators/current-user';
 import { ThingDescription } from './../../common/models/thing-description';
 import { User } from './../../common/models/user';
+import { ConfigService } from './../../config/config.service';
 import { ThingDescriptionDto } from './dto/thing-description.dto';
 import { ThingDescriptionsQueryDto } from './dto/thing-descriptions-query.dto';
 import { ThingsService } from './things.service';
@@ -13,16 +29,18 @@ import { ThingsService } from './things.service';
 @ApiTags('Things')
 @Controller('things')
 export class ThingsController {
-  public constructor(private readonly thingsService: ThingsService) {}
+  public constructor(private readonly config: ConfigService, private readonly thingsService: ThingsService) {}
 
   @Post()
   @UseGuards(AuthGuard)
-  public create(
+  public async create(
     @CurrentUser() user: User,
     @Body() dto: ThingDescriptionDto,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
-    return this.thingsService.create(user, dto, res);
+    const thingDescriptionId = await this.thingsService.create(user, dto);
+    const locationURL = `${this.config.apiBase}/things/${thingDescriptionId}`;
+    res.header('Location', locationURL);
   }
 
   @Get(':id')
@@ -32,28 +50,30 @@ export class ThingsController {
 
   @Put(':id')
   @UseGuards(AuthGuard)
-  public upsert(
+  public async upsert(
     @CurrentUser() user: User,
     @Param('id') id: string,
     @Body() dto: ThingDescriptionDto,
     @Res({ passthrough: true }) res: Response,
   ): Promise<void> {
-    return this.thingsService.upsert(user, id, dto, res);
+    const updated = await this.thingsService.upsert(user, id, dto);
+    res.statusCode = updated ? 204 : 201;
   }
 
   @Patch(':id')
-  public update(
-    @Param('id') id: string,
-    @Body() dto: ThingDescriptionDto,
-    @Req() req: Request,
-    @Res({ passthrough: true }) res: Response,
-  ): Promise<void> {
-    return this.thingsService.update(id, dto, req, res);
+  @HttpCode(204)
+  public update(@Param('id') id: string, @Body() dto: ThingDescriptionDto, @Req() req: Request): Promise<void> {
+    const isCorrectContentType = req.headers['content-type'] === 'application/merge-patch+json';
+    if (!isCorrectContentType) {
+      throw new BadRequestException();
+    }
+    return this.thingsService.update(id, dto);
   }
 
   @Delete(':id')
-  public delete(@Param('id') id: string, @Res({ passthrough: true }) res: Response): Promise<void> {
-    return this.thingsService.delete(id, res);
+  @HttpCode(204)
+  public delete(@Param('id') id: string): Promise<void> {
+    return this.thingsService.delete(id);
   }
 
   @Get()
--- src/api-reference/things/things.service.ts
+++ src/api-reference/things/things.service.ts
@@ -1,35 +1,24 @@
 import { randomUUID } from 'crypto';
 
 import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
-import { FastifyRequest as Request, FastifyReply as Response } from 'fastify';
 import { apply as mergePatch } from 'json-merge-patch';
 
 import { ThingDescription } from './../../common/models/thing-description';
 import { User } from './../../common/models/user';
 import { validateThingDescription } from './../../common/utils/thing-description-validator';
-import { ConfigService } from './../../config/config.service';
 import { ThingDescriptionRepository } from './../../persistence/thing-description.repository';
 import { ThingDescriptionDto } from './dto/thing-description.dto';
 import { ThingDescriptionsQueryDto } from './dto/thing-descriptions-query.dto';
 
 @Injectable()
 export class ThingsService {
-  public constructor(
-    private readonly config: ConfigService,
-    private readonly thingDescriptionRepository: ThingDescriptionRepository,
-  ) {}
+  public constructor(private readonly thingDescriptionRepository: ThingDescriptionRepository) {}
 
-  public async create(user: User, dto: ThingDescriptionDto, res: Response): Promise<void> {
-    const { valid, errors } = validateThingDescription(dto);
-    if (!valid) {
-      throw new BadRequestException({ validationErrors: errors });
-    }
-
-    const uuid = randomUUID();
-    const locationURL = `${this.config.apiBase}/things/${uuid}`;
-
-    await this.thingDescriptionRepository.create({ urn: uuid, json: dto, owner_id: user.id });
-    res.header('Location', locationURL);
+  public async create(user: User, dto: ThingDescriptionDto): Promise<string> {
+    this.requireValidThingDescription(dto);
+    const thingDescriptionId = randomUUID();
+    await this.thingDescriptionRepository.create({ urn: thingDescriptionId, json: dto, owner_id: user.id });
+    return thingDescriptionId;
   }
 
   public async retrieve(id: string): Promise<ThingDescription> {
@@ -40,57 +29,50 @@ export class ThingsService {
     return internalThingDescription.json;
   }
 
-  public async upsert(user: User, id: string, dto: ThingDescriptionDto, res: Response): Promise<void> {
-    const { valid, errors } = validateThingDescription(dto);
-    if (!valid) {
-      throw new BadRequestException({ validationErrors: errors });
-    }
-
+  public async upsert(user: User, id: string, dto: ThingDescriptionDto): Promise<boolean> {
+    this.requireValidThingDescription(dto);
     const internalThingDescription = await this.thingDescriptionRepository.findFirst({ where: { urn: id } });
     if (internalThingDescription) {
       await this.thingDescriptionRepository.update({ where: { id: internalThingDescription.id }, data: { json: dto } });
-      res.statusCode = 204;
+      return true;
     } else {
       await this.thingDescriptionRepository.create({ urn: id, json: dto, owner_id: user.id });
-      res.statusCode = 201;
+      return false;
     }
   }
 
-  public async update(id: string, dto: ThingDescriptionDto, req: Request, res: Response): Promise<void> {
-    const isCorrectContentType = req.headers['content-type'] === 'application/merge-patch+json';
-    if (!isCorrectContentType) {
-      throw new BadRequestException();
-    }
-
+  public async update(id: string, dto: ThingDescriptionDto): Promise<void> {
     const internalThingDescription = await this.thingDescriptionRepository.findFirst({ where: { urn: id } });
     if (!internalThingDescription) {
       throw new NotFoundException();
     }
 
     const patchedThingDescription = mergePatch(internalThingDescription.json, dto);
-    const { valid, errors } = validateThingDescription(patchedThingDescription);
-    if (!valid) {
-      throw new BadRequestException({ validationErrors: errors });
-    }
+    this.requireValidThingDescription(patchedThingDescription);
 
     await this.thingDescriptionRepository.update({
       where: { id: internalThingDescription.id },
       data: { json: patchedThingDescription },
     });
-    res.statusCode = 204;
   }
 
-  public async delete(id: string, res: Response): Promise<void> {
+  public async delete(id: string): Promise<void> {
     const internalThingDescription = await this.thingDescriptionRepository.findFirst({ where: { urn: id } });
     if (!internalThingDescription) {
       throw new NotFoundException();
     }
     await this.thingDescriptionRepository.delete({ where: { id: internalThingDescription.id } });
-    res.statusCode = 204;
   }
 
   public async list(query: ThingDescriptionsQueryDto): Promise<ThingDescription[]> {
-    const thingDescriptions = await this.thingDescriptionRepository.find({});
+    const thingDescriptions = await this.thingDescriptionRepository.find();
     return thingDescriptions.map((td) => td.json);
+  }
+
+  private requireValidThingDescription(data: unknown): void {
+    const { valid, errors } = validateThingDescription(data);
+    if (!valid) {
+      throw new BadRequestException({ validationErrors: errors });
+    }
   }
 }
