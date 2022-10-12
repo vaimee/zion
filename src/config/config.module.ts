f3bee2464c908171163c90f8c06b8a1d98c55ae7
ivan <idimitryz@gmail.com>
2022-10-12 19:35:21 +0200

    feat: NODE_ENV variable determines which .env file will be loaded

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule as NestConfigModule } from '@nestjs/config';
+import { getEnvFilePath } from 'src/common/utils/env-loader';
 
 import { ConfigService } from './config.service';
 import { validationSchema } from './env.validation';
@@ -7,6 +8,7 @@ import { validationSchema } from './env.validation';
 @Module({
   imports: [
     NestConfigModule.forRoot({
+      envFilePath: getEnvFilePath(`${process.cwd()}/src/config/env/`),
       validationSchema,
     }),
   ],
