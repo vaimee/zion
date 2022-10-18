f57151465c529ef4904f75b43c9d7f38924a5abe
ivan <idimitryz@gmail.com>
2022-10-18 13:01:20 +0200

    test: remove manual setup of env variables during test

--- test/env.ts
+++
@@ -1,6 +0,0 @@
-process.env.DB_HOST = 'localhost';
-process.env.DB_PORT = '54310';
-process.env.DB_USER = 'postgres';
-process.env.DB_PASSWORD = 'postgres';
-process.env.DB_DATABASE = 'postgres';
-process.env.JWT_SECRET = 'abc123';
