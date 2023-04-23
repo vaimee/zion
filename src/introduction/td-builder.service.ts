57e55c985e570e62abfb669a259d012cf6844632
Jan Romann <jan.romann@uni-bremen.de>
2023-04-23 10:00:47 +0200

    fix: implement workaround for mandatory response contentType
    Also see https://github.com/w3c/wot-discovery/pull/469

--- src/introduction/td-builder.service.ts
+++ src/introduction/td-builder.service.ts
@@ -27,6 +27,39 @@ export class ThingDescriptionBuilderService {
     // TODO: chose the right security schema using configs
     this.built.securityDefinitions = { nosec: { scheme: 'nosec' } };
     this.built.security = 'nosec';
+
+    // TODO: Make inclusion of fix configurable
+    this.insertResponseContentType();
+
     return this.built;
+  }
+
+  /**
+   * Applies the fix described in section 7.3.2.4 of the Discovery specification to make the
+   * TDD's Thing Description compatible with the TD specification and Consumers performing
+   * validation by including a <code>contentType</code> that is missing from the TM
+   * describing the directory's API.
+   *
+   * TODO: Update URL once the change has been included in the published specification.
+   * @see https://w3c.github.io/wot-discovery/#directory-api-spec
+   */
+  private insertResponseContentType(): void {
+    const actions = this.built?.actions;
+
+    if (actions == null) {
+      return;
+    }
+
+    const actionsToFix = ['createThing', 'createAnonymousThing', 'updateThing', 'partiallyUpdateThing', 'deleteThing'];
+
+    for (const key of actionsToFix) {
+      const invalidResponse = actions[key]?.forms[0].response;
+
+      if (invalidResponse == null) {
+        continue;
+      }
+
+      invalidResponse['contentType'] = 'application/x-empty';
+    }
   }
 }
