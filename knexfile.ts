5940b35e7f68f40ff815087ae477cb7d9496a080
reluc <relu.cri@gmail.com>
2024-02-14 18:31:07 +0100

    fix(knexfile): don't panic if there is no .env file
    When running zion on a container usually the envs
    are provided as configuration parameters from the
    CLI or in a docker-compose file. This change makes
    sure that the knexfile doesn't panic if there is no
    .env file, but uses the process.env directly.

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,5 @@
+import { existsSync } from 'fs';
+
 import { path as rootPath } from 'app-root-path';
 import * as dotenv from 'dotenv';
 import type { Knex } from 'knex';
@@ -5,10 +7,17 @@ import type { Knex } from 'knex';
 const baseEnvPath = `${process.cwd()}`;
 const envPath =
   process.env.NODE_ENV === undefined ? `${baseEnvPath}/.env` : `${baseEnvPath}/${process.env.NODE_ENV}.env`;
-dotenv.config({
-  path: envPath,
-  debug: true,
-});
+
+if (existsSync(envPath)) {
+  dotenv.config({
+    path: envPath,
+    debug: true,
+  });
+} else {
+  console.warn(
+    `No .env file found at ${envPath}, using process.env. If you are running in a container please ensure that the environment variables are set.`,
+  );
+}
 
 const config: Knex.Config = {
   client: 'pg',
